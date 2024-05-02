9b5a995f04a27b73c2a4667898b3f68352294637
Diyaridem <netdiy32@hotmail.com>
2024-05-02 22:42:43 +0300

    Button Head

--- Screens/Products/ProductContainer.js
+++ Screens/Products/ProductContainer.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, FlatListComponent } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Text, ScrollView, FlatList, Image } from 'react-native';
 import ProductCard from './ProductCard';
 import data from '../../assets/data/products.json';
-import { FlatList } from 'react-native-web';
 
 export default function ProductContainer() {
   const [products, setProducts] = useState([]);
@@ -16,14 +15,28 @@ export default function ProductContainer() {
 
   return (
     <View style={styles.container}>
+      <TouchableOpacity onPress={() => console.log('Button pressed')}>
+        <View style={styles.button}>
+          <Image
+            source={require('../../assets/chameleon.png')}
+            style={styles.buttonIcon}
+          />
+          <Text style={styles.buttonText}>Press me</Text>
+        </View>
+      </TouchableOpacity>
       <Text style={styles.heading}>ÜRÜNLER</Text>
-      <ScrollView numColumns={2} contentContainerStyle={styles.productList}>
+      <ScrollView contentContainerStyle={styles.productList}>
         <View style={styles.row}>
-          {products.map((product, index) => (
-            <View key={index} style={styles.productItem}>
-              <ProductCard {...product} />
-            </View>
-          ))}
+          <FlatList
+            data={products}
+            numColumns={2}
+            renderItem={({ item }) => (
+              <View style={styles.productItem}>
+                <ProductCard {...item} />
+              </View>
+            )}
+            keyExtractor={(item, index) => index.toString()}
+          />
         </View>
       </ScrollView>
     </View>
@@ -34,6 +47,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
+    backgroundColor: '#fff',
   },
   heading: {
     fontSize: 20,
@@ -49,7 +63,22 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
   productItem: {
-    width: '48%', // İki kartın yanyana sığması için genişliği azaltıldı
+    width: '48%',
     marginBottom: 10,
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    marginBottom: 10,
+  },
+  buttonIcon: {
+    width: 24,
+    height: 24,
+    marginRight: 10,
+  },
+  buttonText: {
+    fontSize: 16,
   },
 });
