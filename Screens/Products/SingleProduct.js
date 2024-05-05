5704dedcb0e1d7cbfcc9fcc76a109ea013c55d88
Diyaridem <netdiy32@hotmail.com>
2024-05-05 17:42:17 +0300

    Single Product Layout

--- Screens/Products/SingleProduct.js
+++ Screens/Products/SingleProduct.js
@@ -17,8 +17,21 @@ style={styles.image}
 
 />
 </View>
+<View style={styles.contentContainer}>
+                    <H1 style={styles.contentHeader}>{item.name}</H1>
+                    <Text style={styles.contentText}>{item.brand}</Text>
+                </View>
 
 </ScrollView>
+
+<View style={styles.bottomContainer}>
+                <Left>
+                    <Text style={styles.price}>$ {item.price}</Text>
+                </Left>
+                <Right>
+            
+                </Right>
+            </View>
 
         </Container>
     )
@@ -35,11 +48,36 @@ backgroundColor:'white',
 padding:0,
 margin:0
 }
-,
-image:{
-    width:'100%',
-    height:25
-}
+, contentContainer: {
+    marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+},
+contentHeader: {
+    fontWeight: 'bold',
+    marginBottom: 20
+},
+contentText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 20
+},
+image: {
+    width: '100%',
+    height: 250
+},  
+ bottomContainer: {
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    backgroundColor: 'white'
+},
+price: {
+    fontSize: 24,
+    margin: 20,
+    color: 'red'
+},
 })
 
 export default SingleProduct;
