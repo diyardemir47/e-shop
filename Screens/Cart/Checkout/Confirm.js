b2cc183cab090aec2de808b626057150a2a4a777
Diyaridem <netdiy32@hotmail.com>
2024-05-09 11:07:31 +0300

    Confirm

--- Screens/Cart/Checkout/Confirm.js
+++ Screens/Cart/Checkout/Confirm.js
@@ -1,17 +1,72 @@
 import React from "react";
-import {View,Text} from 'react-native';
+import {View,Text,Dimensions,ScrollView} from 'react-native';
+
+import { Left,Text,Right,ListItem,Thumbnail,Body } from "native-base";
+
+import { connect } from "react-redux";
+import * as actions from '../../../Redux/Actions/cartAction'
+
+var {height} =Dimensions.get("window")
+
+const confirm = props.route.params
 
 
-const Confirm=()=> {
+
+
+const Confirm=(props)=> {
 
 return (
 
-    <View>
+ <ScrollView contentContainerStyle={styles.container}>
 
-        <Text>CONFİRM SCREEN</Text>
-    </View>
+<View style={styles.titleContainer}>
+<Text style={{fontSize:20,fontWeight:'bold'}}>
+    Confirm Order
+</Text>
+{props.route.params ?
+<View style={{borderWidth:1,borderColor:'orange'}}>
+<Text style={styles.shipping}>Shipping to:</Text>
+<View style={{padding:8}}> 
+
+<Text>Address: {confirm.order.order.shippingAddress1}</Text>
+<Text>Address2: {confirm.order.order.shippingAddress2}</Text>
+<Text>City: {confirm.order.order.city}</Text>
+<Text>Zip Code: {confirm.order.order.zip}</Text>
+<Text>Country: {confirm.order.order.country}</Text>
+
+</View>
+
+</View>
+: null }
+</View>
+ </ScrollView>
 )
 
 }
+
+const styles=StyleSheet.create({
+
+
+    container:{
+
+height:height,
+padding:8,
+alignContent:'center',
+backgroundColor:'white'
+
+    },
+    titleContainer:{
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    shipping:{
+        alignSelf:'center',
+        margin:8,
+        fontSize:16,
+        fontWeight:'bold'
+
+    }
+})
+
 
 export default Confirm;
