a31ee851e6c16356c0c47b5ee95e9ad3ff4e4844
Diyaridem <netdiy32@hotmail.com>
2024-05-09 22:27:07 +0300

    Activity Container

--- Screens/Products/ProductContainer.js
+++ Screens/Products/ProductContainer.js
@@ -1,42 +1,82 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, ActivityIndicator,Dimensions, TouchableOpacity, View, Text, Image, ScrollView } from 'react-native';
-import ProductCard from './ProductCard';
-import data from '../../assets/data/products.json';
-import { Container, Input, Item, Header, Icon } from 'native-base';
-import SearchedProducts from './SearchedProducts';
-import ProductList from './ProductList';
-import Banner from '../../Shared/Banner';
-import CategoryFilter from './CategoryFilter';
+import React, { useState, useCallback } from "react";
+import {
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+  ScrollView,
+  Dimensions
+} from "react-native";
+import { Container, Header, Icon, Item, Input, Text } from "native-base";
+import { useFocusEffect } from '@react-navigation/native'
+import baseUrl from "../../assets/common/baseUrl"
+import axios from 'axios';
 
-const productCategories = require('../../assets/data/categories.json');
+import ProductList from "./ProductList";
+import SearchedProduct from "./SearchedProducts";
+import Banner from "../../Shared/Banner";
+import CategoryFilter from "./CategoryFilter";
+import baseURL from "../../assets/common/baseUrl";
+
 var { height } = Dimensions.get('window')
-export default function ProductContainer(props) {
+
+const ProductContainer = (props) => {
   const [products, setProducts] = useState([]);
   const [productsFiltered, setProductsFiltered] = useState([]);
-  const [focus, setFocus] = useState(false);
+  const [focus, setFocus] = useState();
   const [categories, setCategories] = useState([]);
   const [productsCtg, setProductsCtg] = useState([]);
   const [active, setActive] = useState();
   const [initialState, setInitialState] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    setProducts(data);
-    setProductsFiltered(data);
-    setFocus(false);
-    setCategories(productCategories);
-    setActive(-1)
-    setInitialState(data)
-    setLoading(false); // Set loading to false when data is loaded
-    return () => {
-      setProducts([]);
-      setProductsFiltered([]);
-      setFocus();
-      setCategories();
-      setInitialState();
-    };
-  }, []);
+  useFocusEffect((
+    useCallback(
+      () => {
+        setFocus(false);
+        setActive(-1);
+        
+        // Products
+        axios
+          .get(`${baseURL}products`)
+          .then((res) => {
+            setProducts(res.data);
+            setProductsFiltered(res.data);
+            setProductsCtg(res.data);
+            setInitialState(res.data);
+            setLoading(false)
+          })
+          .catch((error) => {
+            console.log('Api call error')
+          })
+    
+        // Categories
+        axios
+          .get(`${baseURL}categories`)
+          .then((res) => {
+            setCategories(res.data)
+          })
+          .catch((error) => {
+            console.log('Api call error')
+          })
+    
+        return () => {
+          setProducts([]);
+          setProductsFiltered([]);
+          setFocus();
+          setCategories([]);
+          setActive();
+          setInitialState();
+        };
+      },
+      [],
+    )
+  ))
+    
+   
+  
 
+  // Product Methods
   const searchProduct = (text) => {
     setProductsFiltered(
       products.filter((i) => i.name.toLowerCase().includes(text.toLowerCase()))
@@ -67,101 +107,87 @@ export default function ProductContainer(props) {
 
   return (
     <>
-      {loading ? (
-        // Loading
-        <Container style={[styles.center, { backgroundColor: "#f2f2f2" }]}>
-          <ActivityIndicator size="large" color="red" />
-        </Container>
-      ) : (
-        <Container>
-          <Header searchBar rounded>
-            <Item>
-              <Icon name="ios-search" />
-              <Input
-                placeholder="Search"
-                onFocus={openList}
-                onChangeText={(text) => searchProduct(text)}
-              />
-              {focus == true ? <Icon onPress={onBlur} name="ios-close" /> : null}
-            </Item>
-          </Header>
-          {focus == true ? (
-            <SearchedProducts
-              navigation={props.navigation}
-              productsFiltered={productsFiltered}
-            />
-          ) : (
-            <ScrollView>
-              <View>
-                <View>
-                  <Banner />
-                </View>
-                <View>
-                  <CategoryFilter
-                    categories={categories}
-                    categoryFilter={changeCtg}
-                    productsCtg={productsCtg}
-                    active={active}
-                    setActive={setActive}
-                  />
-                </View>
-                {productsCtg.length > 0 ? (
-                  <View style={styles.listContainer}>
-                    {productsCtg.map((item) => {
-                      return (
-                        <ProductList
-                          navigation={props.navigation}
-                          key={item.name}
-                          item={item}
-                        />
-                      );
-                    })}
-                  </View>
-                ) : (
-                  <View style={[styles.center, { height: height / 2 }]}>
-                    <Text>No products found</Text>
-                  </View>
-                )}
-              </View>
-            </ScrollView>
-          )}
-        </Container>
-      )}
-    </>
+    {loading == false ? (
+ <Container>
+ <Header searchBar rounded>
+   <Item>
+     <Icon name="ios-search" />
+     <Input
+       placeholder="Search"
+       onFocus={openList}
+       onChangeText={(text) => searchProduct(text)}
+     />
+     {focus == true ? <Icon onPress={onBlur} name="ios-close" /> : null}
+   </Item>
+ </Header>
+ {focus == true ? (
+   <SearchedProduct 
+   navigation={props.navigation}
+   productsFiltered={productsFiltered} />
+ ) : (
+   <ScrollView>
+     <View>
+       <View>
+         <Banner />
+       </View>
+       <View>
+         <CategoryFilter
+           categories={categories}
+           categoryFilter={changeCtg}
+           productsCtg={productsCtg}
+           active={active}
+           setActive={setActive}
+         />
+       </View>
+       {productsCtg.length > 0 ? (
+       <View style={styles.listContainer}>
+           {productsCtg.map((item) => {
+               return(
+                   <ProductList
+                       navigation={props.navigation}
+                       key={item.name}
+                       item={item}
+                   />
+               )
+           })}
+       </View>
+       ) : (
+           <View style={[styles.center, { height: height / 2}]}>
+               <Text>No products found</Text>
+           </View>
+       )}
+      
+     </View>
+   </ScrollView>
+ )}
+</Container>
+    ) : (
+      // Loading
+      <Container style={[styles.center, { backgroundColor: "#f2f2f2" }]}>
+        <ActivityIndicator size="large" color="red" />
+      </Container>
+    )}
+   </>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
+    flexWrap: "wrap",
+    backgroundColor: "gainsboro",
+  },
+  listContainer: {
+    height: height,
     flex: 1,
-    padding: 10,
-    backgroundColor: '#fff',
+    flexDirection: "row",
+    alignItems: "flex-start",
+    flexWrap: "wrap",
+    backgroundColor: "gainsboro",
   },
-  heading: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  productList: {
-    flexGrow: 1,
-  },
-  productItem: {
-    width: '48%',
-    marginBottom: 10,
-  },
-  button: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10,
-    marginBottom: 10,
-  },
-  buttonIcon: {
-    width: 24,
-    height: 24,
-    marginRight: 10,
-  },
-  buttonText: {
-    fontSize: 16,
-  },
+  center: {
+      justifyContent: 'center',
+      alignItems: 'center'
+  }
 });
+
+export default ProductContainer;
