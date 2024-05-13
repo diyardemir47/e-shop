6f6872369baf6e6d165d7cadc6e54d90f27e740a
Diyaridem <netdiy32@hotmail.com>
2024-05-13 21:23:47 +0300

    Products

--- Screens/Admin/Products.js
+++ Screens/Admin/Products.js
@@ -1,13 +1,50 @@
-import React from "react"
-import {View,Text} from "react-native";
+import React, { useState, useCallback } from "react";
+import { View, Text, FlatList, ActivityIndicator, StyleSheet, Dimensions, Button } from "react-native";
+import { Header, Item, Input } from "native-base";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { useFocusEffect } from "@react-navigation/native";
+import axios from "axios";
+import AsyncStorage from "@react-native-community/async-storage";
+import baseURL from "../../assets/common/baseUrl";
 
-const Products =(props) => {
+var { height, width } = Dimensions.get("window");
+
+const Products = (props) => {
+    const [productList, setProductList] = useState();
+    const [productFilter, setProductFilter] = useState();
+    const [loading, setLoading] = useState(true);
+    const [token, setToken] = useState();
+
+    useFocusEffect(
+        useCallback(() => {
+            const fetchProducts = async () => {
+                try {
+                    const res = await AsyncStorage.getItem("jwt");
+                    setToken(res);
+                    const response = await axios.get(`${baseURL}products`);
+                    setProductList(response.data);
+                    setProductFilter(response.data);
+                    setLoading(false);
+                } catch (error) {
+                    console.log(error);
+                }
+            };
+
+            fetchProducts();
+
+            return () => {
+                setProductList();
+                setProductFilter();
+                setLoading(true);
+            };
+        }, [])
+    );
 
     return (
         <View>
-            <Text>CATEGORİES SCREEN</Text>
+            <Text>Product SCREEN</Text>
         </View>
-    )
-}
+    );
+};
 
 export default Products;
