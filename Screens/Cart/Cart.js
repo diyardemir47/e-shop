6777c05fc8ab01715af5518a0f64ddd21ab03ffa
Diyaridem <netdiy32@hotmail.com>
2024-05-17 23:08:55 +0300

    Easy Button And TrafficLightAdd

--- Screens/Cart/Cart.js
+++ Screens/Cart/Cart.js
@@ -1,164 +1,172 @@
-import React from 'react'
-import {Button, Dimensions, Text,TouchableOpacity,StyleSheet} from 'react-native'
+import React, { useContext, useEffect, useState } from "react";
+import {
+  View,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import {
+  Container,
+  Text,
+  Left,
+  Right,
+  H1
+} from "native-base";
+import { SwipeListView } from 'react-native-swipe-list-view'
+import CartItem from './CartItem'
 
+import Icon from "react-native-vector-icons/FontAwesome";
+import EasyButton from "../../Shared/StyledComponents/EasyButton"
 
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
+import * as actions from "../../Redux/Actions/cartActions";
+import AuthGlobal from "../../Context/store/AuthGlobal"
+import axios from "axios";
+import baseURL from "../../assets/common/baseUrl";
+import AsyncStorage from "@react-native-community/async-storage"
 
-import Icon from 'react-native-vector-icons/FontAwesome';
-import *as actions from '../../Redux/Actions/cartAction';
-import { Body, Container, Left, ListItem, Right, Thumbnail } from 'native-base';
+var { height, width } = Dimensions.get("window");
 
-import { SwipeListView } from 'react-native-swipe-list-view';
-import CartItem from './CartItem';
+const Cart = (props) => {
 
+  const context = useContext(AuthGlobal);
 
+  // Add this
+  const [productUpdate, setProductUpdate] = useState()
+  const [totalPrice, setTotalPrice] = useState()
+  useEffect(() => {
+    getProducts()
+    return () => {
+      setProductUpdate()
+      setTotalPrice()
+    }
+  }, [props])
+  
+    const getProducts = () => {
+      var products = [];
+      props.cartItems.forEach(cart => {
+        axios.get(`${baseURL}products/${cart.product}`).then(data => {
+          products.push(data.data)
+          setProductUpdate(products)
+          var total = 0;
+          products.forEach(product => {
+            const price = (total += product.price)
+              setTotalPrice(price)
+          });
+        })
+        .catch(e => {
+          console.log(e)
+        })
+      })
+    }
 
-var {height,width}=Dimensions.get("window")
-
-const Cart=(props) => {
-
-    var total=0;
-    props.cartItems.forEach(cart=>{
-        return (total +=cart.product.price)
-    });
-
-    return (
-        
-      <>
-      {props.cartItems.length ? (
+  return (
+    <>
+      {productUpdate ? (
         <Container>
-<H1 style={{alignSelf:'center'}}>
-
-    <SwipeListView  data={props.cartItems}
-    renderItem={(data)=>{
-        <CartItem item={data} />
-    }}
-    renderHiddenItem={(data)=>{
-        <View style={styles.hiddenContainer}>
-        <TouchableOpacity 
+          <H1 style={{ alignSelf: "center" }}>Cart</H1>
+          <SwipeListView
+            data={productUpdate}
+            renderItem={(data) => (
+             <CartItem item={data} />
+            )}
+            renderHiddenItem={(data) => (
+              <View style={styles.hiddenContainer}>
+                <TouchableOpacity 
                 style={styles.hiddenButton}
                 onPress={() => props.removeFromCart(data.item)}
                 >
-                <Icon 
-                name='trash'
-                color={"white"}
-                size={30}
-                />
-            </TouchableOpacity>
-        </View>
-    }}
-
-    disableRightSwipe={true}
-    previewOpenDelay={3000}
-    friction={1000}
-    tension={40}
-    leftOpenValue={75}
-    stopLeftSwipe={75}
-    rightOpenValue={-75}
-    />
-
-
-</H1>
-{props.cartItems.map(data=>{
-    return (
-        <ListItem style={styles.listItem}
-        key={Math.random()}
-        avatar
-        >
-<Left>
-    <Thumbnail source={{uri:data.product.image ? data.product.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'}}/>
-
- 
-</Left>
-<Body style={styles.body}>
-<Left>
-    <Text>{data.product.name}</Text>
-</Left>
-<Right>
-    <Text>
-        $ {data.product.price}
-    </Text>
-</Right>
-</Body>
-        </ListItem>
-    )
-})}
-<View>
-<Left>
-<Text style={styles.price}>
-$ {total}
-</Text>
-
-</Left>
-
-<Right>
-    <Button  title="Clear"
-    onPress={()=>props.clearCart()}
-    />
-</Right>
-<Right>
-    <Button  title="Checkout" onPress={()=>props.navigation.navigate('Checkout')}/>
-</Right>
-</View>
-
+                  <Icon name="trash" color={"white"} size={30} />
+                </TouchableOpacity>
+              </View>
+            )}
+            disableRightSwipe={true}
+            previewOpenDelay={3000}
+            friction={1000}
+            tension={40}
+            leftOpenValue={75}
+            stopLeftSwipe={75}
+            rightOpenValue={-75}
+          />
+          <View style={styles.bottomContainer}>
+            <Left>
+                <Text style={styles.price}>$ {totalPrice}</Text>
+            </Left>
+            <Right>
+                <EasyButton
+                  danger
+                  medium
+                  onPress={() => props.clearCart()}
+                >
+                  <Text style={{ color: 'white' }}>Clear</Text>
+                </EasyButton>
+            </Right>
+            <Right>
+              {context.stateUser.isAuthenticated ? (
+                <EasyButton
+                  primary
+                  medium
+                  onPress={() => props.navigation.navigate('Checkout')}
+                >
+                <Text style={{ color: 'white' }}>Checkout</Text>
+                </EasyButton>
+              ) : (
+                <EasyButton
+                  secondary
+                  medium
+                  onPress={() => props.navigation.navigate('Login')}
+                >
+                <Text style={{ color: 'white' }}>Login</Text>
+                </EasyButton>
+              )}
+                
+            </Right>
+          </View>
         </Container>
-      ):
-      (
-        <Container style={StyleSheet.emptyContainer}>
-            <Text>LOOKS LİKE YOUR CART İS EMPTY</Text>
-            <Text>Add product to your cart to get started</Text>
+      ) : (
+        <Container style={styles.emptyContainer}>
+          <Text>Looks like your cart is empty</Text>
+          <Text>Add products to your cart to get started</Text>
         </Container>
-      )
-      }
-      </>
-    )
-}
+      )}
+    </>
+  );
+};
 
-const mapStateToProps=(state)=>{
-    const {cartItems}=state;
-    return{
-        cartItems:cartItems
-    };
+const mapStateToProps = (state) => {
+  const { cartItems } = state;
+  return {
+    cartItems: cartItems,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-      clearCart: () => dispatch(actions.clearCart()),
-      removeFromCart: (item) => dispatch(actions.removeFromCart(item))
-      }
-  }
-
-const styles=StyleSheet.create({
-    emptyContainer:{
-height:height,
-alignItems:'center',
-justifyContent:'center'
-
-    },
-    listItem:{
-        alignItems:'center',
-        backgroundColor:'white',
-        justifyContent:'center'
-    },
-    body:{
-        margin:10,alignItems:'center',
-        flexDirection:'row'
-    },
-    bottomContainer:{
-        flexDirection:'row',
-        position:'absolute',
-        bottom:0,
-        left:0,
-        backgroundColor:'white',
-        elevation:0,
-
+  return {
+    clearCart: () => dispatch(actions.clearCart()),
+    removeFromCart: (item) => dispatch(actions.removeFromCart(item))
     }
-,price:{
-    fontSize:18,
-    mardin:20,
-    color:'red'
-},
-hiddenContainer: {
+}
+
+const styles = StyleSheet.create({
+  emptyContainer: {
+    height: height,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bottomContainer: {
+      flexDirection: 'row',
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      backgroundColor: 'white',
+      elevation: 20
+  },
+  price: {
+      fontSize: 18,
+      margin: 20,
+      color: 'red'
+  },
+  hiddenContainer: {
     flex: 1,
     justifyContent: 'flex-end',
     flexDirection: 'row'
@@ -171,6 +179,6 @@ hiddenContainer: {
     height: 70,
     width: width / 1.2
   }
-})
+});
 
-export default   connect(mapStateToProps,mapDispatchToProps) (Cart);
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
--- Screens/User/Login.js
+++ Screens/User/Login.js
@@ -1,91 +1,84 @@
-import React ,{ useContext,useEffect,useState} from 'react';
+import React, { useEffect, useContext, useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import FormContainer from "../../Shared/Form/FormContainer";
+import Input from "../../Shared/Form/Input";
+import Error from "../../Shared/Error";
+import EasyButton from "../../Shared/StyledComponents/EasyButton";
 
-import { View,Text,StyleSheet,Button } from 'react-native';
+// Context
+import AuthGlobal from "../../Context/store/AuthGlobal";
+import { loginUser } from "../../Context/actions/Auth.actions";
 
-import FormContainer from '../../Shared/Form/FormContainer';
+const Login = (props) => {
+  const context = useContext(AuthGlobal);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-import Input from '../../Shared/Form/Input';
-import Error from '../../Shared/Error';
+  useEffect(() => {
+    if (context.stateUser.isAuthenticated === true) {
+      props.navigation.navigate("User Profile");
+    }
+  }, [context.stateUser.isAuthenticated]);
 
-//Context
-import AuthGlobal from '../../Context/store/AuthGlobal';
-
-import {loginUser} from "../../Context/actions/Auth.action"
-
-const Login=(props) => {
-
-
-    const context = useContext(AuthGlobal);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-  
-    useEffect(() => {
-      if (context.stateUser.isAuthenticated === true) {
-        props.navigation.navigate("User Profile");
-      }
-    }, [context.stateUser.isAuthenticated]);
-  
-    const handleSubmit = () => {
-      const user = {
-        email,
-        password,
-      };
-  
-      if (email === "" || password === "") {
-        setError("Please fill in your credentials");
-      } else {
-        loginUser(user, context.dispatch);
-      }
+  const handleSubmit = () => {
+    const user = {
+      email,
+      password,
     };
 
-    return (
+    if (email === "" || password === "") {
+      setError("Please fill in your credentials");
+    } else {
+      loginUser(user, context.dispatch);
+    }
+  };
 
-        <FormContainer title={"Login"}>
-        <Input
-          placeholder={"Enter Email"}
-          name={"email"}
-          id={"email"}
-          value={email}
-          onChangeText={(text) => setEmail(text.toLowerCase())}
-        />
-        <Input
-          placeholder={"Enter Password"}
-          name={"password"}
-          id={"password"}
-          secureTextEntry={true}
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-        />
-        <View style={styles.buttonGroup}>
-          {error ? <Error message={error} /> : null}
-          <EasyButton large primary onPress={() => handleSubmit()}>
-            <Text style={{ color: "white" }}>Login</Text>
-          </EasyButton>
-        </View>
-        <View style={[{ marginTop: 40 }, styles.buttonGroup]}>
-          <Text style={styles.middleText}>Don't have an account yet?</Text>
-          <EasyButton
-          large
-          secondary 
-          onPress={() => props.navigation.navigate("Register")}>
-            <Text style={{ color: "white" }}>Register</Text>
-          </EasyButton>
-        </View>
-      </FormContainer>
-    )
-
-} 
+  return (
+    <FormContainer title={"Login"}>
+      <Input
+        placeholder={"Enter Email"}
+        name={"email"}
+        id={"email"}
+        value={email}
+        onChangeText={(text) => setEmail(text.toLowerCase())}
+      />
+      <Input
+        placeholder={"Enter Password"}
+        name={"password"}
+        id={"password"}
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+      <View style={styles.buttonGroup}>
+        {error ? <Error message={error} /> : null}
+        <EasyButton large primary onPress={() => handleSubmit()}>
+          <Text style={{ color: "white" }}>Login</Text>
+        </EasyButton>
+      </View>
+      <View style={[{ marginTop: 40 }, styles.buttonGroup]}>
+        <Text style={styles.middleText}>Don't have an account yet?</Text>
+        <EasyButton
+        large
+        secondary 
+        onPress={() => props.navigation.navigate("Register")}>
+          <Text style={{ color: "white" }}>Register</Text>
+        </EasyButton>
+      </View>
+    </FormContainer>
+  );
+};
 
 const styles = StyleSheet.create({
-    buttonGroup: {
-      width: "80%",
-      alignItems: "center",
-    },
-    middleText: {
-      marginBottom: 20,
-      alignSelf: "center",
-    },
-  });
+  buttonGroup: {
+    width: "80%",
+    alignItems: "center",
+  },
+  middleText: {
+    marginBottom: 20,
+    alignSelf: "center",
+  },
+});
 
 export default Login;
